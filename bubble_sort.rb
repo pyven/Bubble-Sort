5d1551494d7dd135c8dd98b238eab46315e59284
pyven <quimegson@gmail.com>
2020-01-20 23:58:30 +0800

    fixed a few errors and loop statements

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,18 @@
 def bubble_sort(arr)
   isSorted = false
-  while !isSorted do
-    for i in 1..arr.length-1 
+  j = 0
+
+  while j < arr.length - j do
+    for i in 1...arr.length - j
       if arr[i-1] > arr[i]
         arr[i - 1], arr[i] = arr[i], arr[i - 1]
-        isSorted = true
-      else
-        isSorted = false
       end
     end
+    j++
   end
   return arr
 end
+=begin
+array = [5, 4 ,3, 2, 1]
+p bubble_sort(array)
+=end
